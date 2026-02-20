031597a9d024ccb7c62b235e863567093d1eda31
josue-balbontin <balbontinjosue@gmail.com>
2026-02-19 20:22:37 -0400

    agregando todos los metodos pedidos del pdf

--- Code/UTP/class/Seabattleagent.cpp
+++ Code/UTP/class/Seabattleagent.cpp
@@ -8,14 +8,42 @@ class SeabattleAgent{
     private:
         SeabattleField mitablero;
         SeabattleField oponentetablero;
+        int socket;
     public:
         void init(const SeabattleField &tablero){
             mitablero = tablero;
             oponentetablero = SeabattleField(mitablero.size());
         }
 
-        void star_game(){
+        void startClient(){
+            //logica para iniciar el cliente, conectarse al servidor y manejar la comunicación 
+        }
+
+        void startServer(){
+            //logica para iniciar el servidor, esperar conexiones y manejar la comunicación
+        }
+
+        
+
+        void star_game(bool iniciador){
             // bucle principal del juego, alternando turnos entre el jugador y el oponente, procesando disparos y actualizando los tableros
+            while(!is_game_ended()){
+                if(iniciador){
+                    // turno del jugador
+                    // 1. Leer movimiento del jugador
+                    // 2. Enviar movimiento al oponente
+                    // 3. Leer resultado del movimiento
+                    // 4. Actualizar tablero del oponente con el resultado
+                }else{
+                    // turno del oponente
+                    // 1. Leer movimiento del oponente
+                    // 2. Procesar disparo en el tablero propio
+                    // 3. Enviar resultado al oponente
+                    // 4. Actualizar tablero propio con el resultado
+                }
+                iniciador = !iniciador; // alternar turnos
+            }
+        
         }
 
         pair<int, int> parse_move(string move){
@@ -30,6 +58,24 @@ class SeabattleAgent{
         bool is_game_ended(){
             //verifica si el juego ha terminado
         }
+
+        void ReadMove(){
+            // recibir movimiento del oponente 
+        }
+
+        void ReadResult(){
+            // recibir resultado del movimiento 
+        }
+
+        void SendMove(int x, int y){
+            // enviar movimiento al oponente 
+        }
+
+        void SendResult(string result){
+            // enviar resultado del movimiento al oponente 
+        }
+
+
 
 
     

483bf777e514bc570f3bf49d4b05fc0410f7c695
josue-balbontin <balbontinjosue@gmail.com>
2026-03-20 10:52:53 -0400

    completando api

--- Code/blocks/home/blog/blog.js
+++ Code/blocks/home/blog/blog.js
@@ -1,33 +1,12 @@
 import { AppTarjeta } from "../../app-tarjeta/app-tarjeta.js";
+import { ApiBase } from "../../../services/api/ApiBase.js";
+import { tarjetaVertical } from "../../../services/tarjetaFactory.js";
 
 const html =/*html*/`
   <section class="blog">
         <h1 id="blog">Blog</h1>
             <div class="blog__lista">
-                <app-tarjeta>
-                    <img slot="imagen" src="img/iamed.webp"> 
-                    <div slot="contenido">
-                        <h2></h2>
-                        <p></p>
-                    </div>
-                </app-tarjeta>
-
-                <app-tarjeta>
-                    <img slot="imagen" src="img/patrones.png">
-                    <div slot="contenido">
-                        <h2></h2>
-                        <p></p>
-                    </div>
-                </app-tarjeta>
-
-                <app-tarjeta>
-                    <img slot="imagen" src="img/x86.webp" >
-                    <div slot="contenido">
-                        <h2></h2>
-                        <p></p>
-                    </div>
-                </app-tarjeta>
-
+  
         
             </div>
         <button class="boton boton--informacion">Ver Blog</button>
@@ -39,14 +18,35 @@ const html =/*html*/`
 export class Blog extends HTMLElement {
     constructor() {
         super();
+        this.url = './data/blog.json';
+        
+    }
+
+    async connectedCallback(){
+        
         this.crearBlog();
+
+    
+        const blogData = await this.obtenerBlog();
+            
+        const tarjetasHTML = blogData.slice(0, 3).map(element => {
+            return tarjetaVertical.crearTarjeta(element);
+        }).join('');
+
+            this.querySelector('.blog__lista').innerHTML = tarjetasHTML;
+        
+    
     }
 
     crearBlog() {
         this.innerHTML = html;
     }
+    
 
 }
+
+
+Object.assign(Blog.prototype, ApiBase);
 
 customElements.define('app-blog', Blog);
 

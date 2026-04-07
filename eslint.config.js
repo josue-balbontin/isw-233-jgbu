955edef683cea366c329e09f13d5e1a9b5adbdda
josue-balbontin <balbontinjosue@gmail.com>
2026-04-06 22:43:36 -0400

    arreglando dependencias

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        customElements: "readonly",
+        HTMLElement: "readonly"
+      }
+    },
+    rules: {
+
+    }
+  }
+];

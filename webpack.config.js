f5a40375e6656248ba6fb294720604f7c341f962
josue-balbontin <balbontinjosue@gmail.com>
2026-04-07 09:34:13 -0400

    configurando webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default (env, argv) => {
-
   const isProduction = argv.mode === 'production';
 
   return {
@@ -17,27 +17,75 @@ export default (env, argv) => {
       filename: isProduction ? '[name].[contenthash].js' : '[name].js',
       clean: true, 
     },
-
     devServer: {
       static: path.resolve(__dirname, 'dist'),
       hot: true,
       open: true,
       port: 3000,
     },
-
     devtool: isProduction ? false : 'source-map',
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/index.html', 
+        template: './src/index.html',
         minify: isProduction ? {
           collapseWhitespace: true,
           removeComments: true,
-        } : false, 
+        } : false,
+      }),
+
+      new MiniCssExtractPlugin({
+        filename: isProduction ? '[name].[contenthash].css' : '[name].css',
       }),
     ],
     module: {
       rules: [
-     
+
+        {
+          test: /\.html$/i,
+          loader: 'handlebars-loader',
+          options: {
+            partialDirs: [path.resolve(__dirname, 'src/partials')],
+          },
+        },
+
+        {
+          test: /\.css$/i,
+          resourceQuery: /inline/,
+          use: ['postcss-loader'],
+          type: 'asset/source', 
+        },
+
+        {
+          test: /\.css$/i,
+          resourceQuery: { not: [/inline/] }, 
+          use: [
+            MiniCssExtractPlugin.loader, 
+            'css-loader', 
+            'postcss-loader'
+          ],
+        },
+
+        {
+          test: /\.(png|jpe?g|gif|svg|webp)$/i,
+          type: 'asset', 
+          generator: {
+            filename: 'images/[name].[contenthash][ext]',
+          },
+        },
+
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name].[contenthash][ext]',
+          },
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [
+        '...', 
+        new CssMinimizerPlugin(), 
       ],
     },
   };

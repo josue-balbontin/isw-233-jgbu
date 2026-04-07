c61d3a72a304e01d7d103ad89738b27f3282e85e
josue-balbontin <balbontinjosue@gmail.com>
2026-04-06 20:55:41 -0400

    ajustando nombres y estructura

---
+++ src/blocks/home/__experiencia/home__experiencia.js
@@ -0,0 +1,82 @@
+import imgPasa from './img/pasa.png';
+import imgArqui from './img/arqui.png';
+import imgTickets from './img/tickets.png';
+import imgInspection from './img/inds_inspectionpng.png';
+import imgWhitebelt from './img/whitebelt.png';
+import imgGoogle from './img/google.png';
+
+
+const proyectos = /*html*/`<section class="experiencia">
+        <h1 id="proyectos">Proyectos Destacados</h1>
+        <div class="experiencia__lista">
+            <div class="experiencia__proyecto">
+                <div>
+                    <h2>Aplicación de Reserva de buses</h2>
+                    <p>Aplicación de gestión de flota de buses desarrollada en Python...</p>
+                </div>
+                <img class="experiencia__imagen" src="${imgPasa}" alt="Captura de pantalla del Proyecto 1">
+            </div>
+            
+            <div class="experiencia__proyecto">
+                <img class="experiencia__imagen" src="${imgArqui}" alt="Captura de pantalla del Proyecto 2">     
+                <div>
+                    <h2>Simulador de Arquitectura x86</h2>
+                    <p>Simulador de Arquitectura x86 desarrollado en Microsoft Excel...</p>
+                </div> 
+            </div>
+
+            <div class="experiencia__proyecto">
+                <div>
+                    <h2>Plataforma de ventas y gestion de tickets de eventos</h2>
+                    <p>Sistema integral de gestión de eventos y venta de entradas desarrollado...</p>
+                </div>
+                <img class="experiencia__imagen" src="${imgTickets}" alt="Captura de pantalla del Proyecto 3">
+            </div>
+            <button class="boton boton--informacion">Ver más proyectos</button>
+        </div>
+    </section>`;
+
+const experiencia = /*html*/`<section class="experiencia">
+        <h1 id="certificados">Certificados</h1>
+        <div class="experiencia__lista">
+            <div class="experiencia__proyecto">
+                <div>
+                    <h2>Computer Vision for Industrial Inspection</h2>
+                    <p>Implementación de pipelines de visión por computadora...</p>
+                </div>
+                <img class="experiencia__imagen" src="${imgInspection}" alt="Certificado 1">
+            </div>
+            
+            <div class="experiencia__proyecto">
+                <img class="experiencia__imagen" src="${imgWhitebelt}" alt="Certificado 2">     
+                <div>
+                    <h2>Lean Six Sigma White Belt</h2>
+                    <p>Comprensión de los fundamentos de la metodología Lean Six Sigma...</p>
+                </div> 
+            </div>
+
+            <div class="experiencia__proyecto">
+                <div>
+                    <h2>Google IA SUMMIT for Education</h2>
+                    <p>Participación en la conferencia internacional sobre la integración de la IA...</p>
+                </div>
+                <img class="experiencia__imagen" src="${imgGoogle}" alt="Certificado 3">
+            </div>
+            <button class="boton boton--informacion">Ver más certificados</button>
+        </div>
+    </section>`;
+
+
+export class Experiencia extends HTMLElement {
+    constructor() {
+        super();
+        this.crearExperiencia();
+    }
+
+    crearExperiencia() {
+        this.innerHTML = proyectos + experiencia;
+    }
+
+}
+
+customElements.define('app-experiencia', Experiencia);
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { Presentacion } from "./blocks/home/presentacion/presentacion.js";
-import { SobreMi } from "./blocks/home/sobre-mi/sobre-mi.js";
-import { Blog } from "./blocks/home/blog/blog.js";
-import { Experiencia } from "./blocks/home/experiencia/experiencia.js";
+import './index.css';
+
+import { Presentacion } from "./blocks/home/__presentacion/home__presentacion.js";
+import { SobreMi } from "./blocks/home/__sobre-mi/home__sobre-mi.js";
+import { Blog } from "./blocks/home/__blog/home__blog.js";
+import { Experiencia } from "./blocks/home/__experiencia/home__experiencia.js";
 import router from "./services/router.js";
 import { Home } from "./blocks/home/home.js";
 import { PageAbout } from "./blocks/page-about/page-about.js";
